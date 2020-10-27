caa6cc39e31bdbfdc20b058730738c2646835d19
Hamilton <prof.hamiltonmachiti@usjt.br>
2020-10-27 20:49:09 -0300

    Finalizando projeto

---
+++ src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Cliente } from '../cliente.model';
+import { ClienteService } from '../cliente.service';
+import { Subscription, Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-cliente-lista',
+  templateUrl: './cliente-lista.component.html',
+  styleUrls: ['./cliente-lista.component.css'],
+})
+export class ClienteListaComponent implements OnInit, OnDestroy {
+
+
+  clientes: Cliente[] = [];
+  private clientesSubscription: Subscription;
+
+  constructor(public clienteService: ClienteService) {
+
+  }
+
+  ngOnInit(): void {
+    this.clientes = this.clienteService.getClientes();
+    this.clientesSubscription = this.clienteService.getListaDeCientesAtualizadaObservable().subscribe((clientes:Cliente[])=>{
+      this.clientes = clientes;
+    });
+  }
+
+  ngOnDestroy(): void{
+    this.clientesSubscription.unsubscribe();
+  }
+}
